b8c05e2b30c13b43f3a2f963fbbc8be38941b965
Omega0x0 <OmegaHex@yandex.ru>
2022-08-09 16:28:42 +0300

    Adding comments to the Token enum

--- src/token.rs
+++ src/token.rs
@@ -1,12 +1,12 @@
 pub enum Token {
-    CommentLine,
-    Increment,
-    Decrement,
-    MoveLeft,
-    MoveRight,
-    BlockCode(Vec<Token>),
-    Loop(Vec<Token>),
-    CreateFunction,
-    CallFunction,
-    Debug
+    CommentLine,            // ';'
+    Increment,              // '+'
+    Decrement,              // '-'
+    MoveLeft,               // '<'
+    MoveRight,              // '>'
+    BlockCode(Vec<Token>),  // {...}
+    Loop(Vec<Token>),       // [...]
+    CreateFunction,         // '$'
+    CallFunction,           // '*'
+    Debug                   // '!'
 }
