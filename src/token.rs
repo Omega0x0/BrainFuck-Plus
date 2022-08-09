7fac8222284ac8f9c7f95c1545408dcb5d2e2026
Omega0x0 <OmegaHex@yandex.ru>
2022-08-09 17:32:54 +0300

    Implementing trait toString for enum Token

--- src/token.rs
+++ src/token.rs
@@ -12,3 +12,30 @@ pub enum Token {
     CallFunction,           // '*'
     Debug                   // '!'
 }
+
+impl ToString for Token {
+    fn to_string(&self) -> String {
+        let mut res = String::new();
+
+        match self {
+            Token::Increment => res.push_str(":Increment:"),
+            Token::Decrement => res.push_str(":Decrement:"),
+            Token::MoveLeft => res.push_str(":MoveLeft:"),
+            Token::MoveRight => res.push_str(":MoveRight:"),
+            Token::Print => res.push_str(":Print:"),
+            Token::Input => res.push_str(":Input:"),
+            Token::BlockCode(tokens) | 
+            Token::Loop(tokens) => {
+                for token in tokens.iter() {
+                    res.push_str(token.to_string().as_str())
+                }
+            },
+            Token::CreateFunction => res.push_str(":CreateFunction:"),
+            Token::CallFunction => res.push_str(":CallFunction:"),
+            Token::Debug => res.push_str(":Debug:"),
+            _ => {}
+        }
+
+        res
+    }
+}
