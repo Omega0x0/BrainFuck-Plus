3ad4f4c2c22df7da0b543811fdf1d017a27fabdf
Omega0x0 <OmegaHex@yandex.ru>
2022-08-10 18:46:03 +0300

    Working version of the interpreter BFP (v0.0.1)

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
-fn main() {}
+use std::env;
+
+use brainfuck_plus_interpreter::project_builder::Project;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let path_project = args[1].clone();
+
+    let project = Project::init(path_project);
+    project.build();
+}
